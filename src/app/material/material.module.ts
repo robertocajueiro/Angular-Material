4eefe8dd0cd2acb4b965ffd9b3498bba9dfd6d70
robertocajueiro <beto.cajueiro@gmail.com>
2023-02-16 10:18:50 -0300

    Sidenav e Toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    MatSlideToggleModule
+
   ],
   exports: [
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule { }
